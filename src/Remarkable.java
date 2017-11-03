3a841ec4fdcb8ef9050a6a02fbac45476235a6e3
Kendra Kleber <league@Mac-mini-2.local>
2017-11-02 18:31:07 -0700

    remarkable,secret message box
    11/02/17

--- src/Remarkable.java
+++ src/Remarkable.java
@@ -1,12 +1,21 @@
 import javax.swing.JOptionPane;
 
 public class Remarkable {
-public static void main(String[] args) {
-	String Remarkable = JOptionPane.showInputDialog(" what's your name "); 
+	public static void main(String[] args) {
+		String remarkable = JOptionPane.showInputDialog(" what's your name ");
+		if (remarkable.equalsIgnoreCase("Josh")) {
+			JOptionPane.showMessageDialog(null, "cool shoes");
+		} else if (remarkable.equalsIgnoreCase("Marc")) {
+			JOptionPane.showMessageDialog(null, "nice hat");
+		} else if (remarkable.equalsIgnoreCase("Jon")) {
+			JOptionPane.showMessageDialog(null, "where are you");
+		} else if (remarkable.equalsIgnoreCase("Maran")) {
+			JOptionPane.showMessageDialog(null, "hi");
+		} else if (remarkable.equalsIgnoreCase("Inez")) {
+			JOptionPane.showMessageDialog(null, "nice hair");
 
+		} else
+			JOptionPane.showMessageDialog(null, "Who are you");
 
-
-
-
-}
+	}
 }
--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -1,32 +1,18 @@
-//    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
-
-
 import javax.swing.JOptionPane;
-
-/**
- * Secret Message Box / Secure Messaging System
- * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
- * 
- */
+import javax.swing.Popup;
 
 public class SecretMessageBox {
+	public static void main(String[] args) {
+		String passcode = "Loading...";
+		String message =  JOptionPane.showInputDialog(null, "Type the secret message");
+		passcode = JOptionPane.showInputDialog(" what's the password ");
+		if (passcode.equalsIgnoreCase(passcode)) {
+			JOptionPane.showMessageDialog(null, message);}
+		
+	else 
 
-	// 0. Make a main method and put steps 1-5 inside it
+			JOptionPane.showMessageDialog(null, "INTRUDER!!!");
+		
+	}
 	
-	// 1. Set the passcode in a String variable
-	
-	// 2. Using a pop-up, ask for a secret message and store it in a variable
-
-	// 3. Ask your friend for the passcode and store it in a variable
-
-	// 4. If the passcode matches, show the secret message
-
-	// 5. If the passcode does not match, pop-up "INTRUDER!!"
-
-	// [optional] 6. Have your friend also enter a username, and make sure it is correct before releasing the secret message.
-
 }
