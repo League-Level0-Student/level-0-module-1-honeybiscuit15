0ce7e1e7d8b3f733b74f69ccead728ca52b98dbd
honeybiscuit15 <league@10.18.34.1>
2017-11-16 18:26:02 -0800

    riddlerip

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -4,12 +4,35 @@ public class TheRiddler {
 
 	public static void main(String[] args) {
 		 int score = 0 ;  
-		 String guess;
 		 String answer = "secret"; 
 		String message =  JOptionPane.showInputDialog(null, "If you have me, you want to share me. If you share me, you haven't got me. What am I?");
-		if (guess.equals(score)) {
-			JOptionPane.showMessageDialog(null, message);}		
+		if (message.equalsIgnoreCase(answer)) {
+			JOptionPane.showMessageDialog(null, "awesome");
+			score++; 
+			JOptionPane.showMessageDialog(null, "you get a point" );
 		
+		}		
+		
+		else{
+			JOptionPane.showMessageDialog(null, "loading..");}
+		 
+	 answer = "tomorrow"; 
+			 message =  JOptionPane.showInputDialog(null, "What is always coming but never arrives?");
+			if (message.equalsIgnoreCase(answer)) {
+				JOptionPane.showMessageDialog(null, "awesome");
+				score++; 
+				JOptionPane.showMessageDialog(null, "you get a point" );
+			
+			}		
+			
+			else
+				JOptionPane.showMessageDialog(null, "loading..");
+		
+	JOptionPane.showMessageDialog(null, "you scored..")+score;
 	}
+	
 }
+	
+	
+
 
